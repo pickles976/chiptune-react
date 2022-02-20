45427be526a1451e00af76042542af669222c65e
Sebastian <sebaslogo@gmail.com>
2022-02-20 13:27:49 -0600

    User can now save songs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useEffect, useState} from "react";
-import {DragDropContext,Droppable,Draggable} from "react-beautiful-dnd";
-import {Midi} from "@tonejs/midi";
+import React from "react";
 import "./DraggableList.jsx";
 import DraggableList from './DraggableList.jsx';
+import {Buffer} from 'buffer';
 import axios from "axios";
-
 import {Transport, Draw, getDestination} from "tone";
 import {getMidi, getInstruments, getNotes, getParts} from "./midi.js";
 import {getAnalysers, allContext, drawWave} from "./visualize";
-import { Tone } from 'tone/build/esm/core/Tone';
 
 function App() {
 
-  let instrumentNums = [81,81,40,119];
-  let perc = [false,false,false,true];
-  let instrument = {                   // and object representing the program change events
-    number : 81,              // the instrument number 0-127
-    family: "Lead",               // the family of instruments, read only.
-    name : "Synth",                // the name of the instrument
-    percussion: false          // if the instrument is a percussion instrument
-  }
+  let instrumentNums = [80,80,39,118];
+  // let instrumentNames = ["synth lead", "synth lead", "strings", "percussive"]
+  // let instrument = {                   // and object representing the program change events
+  //   family: "Lead",               // the family of instruments, read only.
+  //   number : 81,              // the instrument number 0-127
+  //   name : "Synth"                // the name of the instrument
+  // }
 
   let instruments = [];
   let notes = [];
@@ -96,17 +91,21 @@ function App() {
     }
   }
 
+  // download the midi
   function downloadMidi(){
-    midi.tracks.forEach((track,index) => {
-      let temp = instrument;
-      temp.number = instrumentNums[mapping[index]];
-      temp.percussion = perc[mapping[index]];
-      midi.tracks[index].instrument = temp;
-    })
-    let temp = midi.toArray();
-    // TODO: Node.js server to save off data
-    // console.log(new Midi(temp))
-    axios.post('https://localhost:8000/postMidi', midi.toArray()).then(response => console.log(response));
+    console.log(midi)
+
+    for(let i = 0; i < midi.tracks.length;i++){
+      midi.tracks[i].instrument.number = instrumentNums[mapping[i]]
+      midi.tracks[i].channel = 3-i
+    }
+    const url = window.URL.createObjectURL(new Blob([midi.toArray()]));
+    const link = document.createElement('a');
+    link.href = url;
+    // TODO: Allow user to name their song
+    link.setAttribute('download', 'song.mid');
+    document.body.appendChild(link);
+    link.click();
   }
 
   return (
